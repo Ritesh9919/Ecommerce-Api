c9bd301f5b32d832cfaa6278f05481a361596d6e
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-08-19 22:23:47 +0530

    created router and contrller for register,login and logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,31 +1,54 @@
 
 const User = require('../models/user');
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const BadRequestError = require('../errors/bad_request');
+const UnauthenticatedError = require('../errors/unauthenticated_error');
+const createJwtAndVarify = require('../utils/jwt');
 
-module.exports.register = async(req, res) => {
-    const {name, email, password} = req.body;
-    const isEmailAlreadyExist = await User.findOne({email});
-    if(isEmailAlreadyExist) {
-      throw new BadRequestError('Email already exist');
+module.exports.register = async (req, res) => {
+    const { name, email, password } = req.body;
+    const isEmailAlreadyExist = await User.findOne({ email });
+    if (isEmailAlreadyExist) {
+        throw new BadRequestError('Email already exist');
     }
 
     // first user registred as admin
     const isFirstAccount = (await User.countDocuments({})) === 0;
-    const role = isFirstAccount ? 'admin':'user';    
-    const user = await User.create({
-        name,
-        email,
-        password,
-        role});
-    return res.status(StatusCodes.CREATED).json({user});
+    const role = isFirstAccount ? 'admin' : 'user';
+    const user = await User.create({ name, email, password, role });
+
+    const tokenUser = { name: user.name, id: user._id, role: user.role };
+    createJwtAndVarify.attatchCookiesToResponse({res, user:tokenUser});
+    return res.status(StatusCodes.CREATED).json({ user: tokenUser});
 }
 
-module.exports.login = (req, res) => {
-    return res.send('Log in user');
+module.exports.login = async(req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password) {
+        throw new BadRequestError('please provide email and password');
+    }
+    const user = await User.findOne({email});
+    if(!user) {
+        throw new UnauthenticatedError('Invalid credentials');
+    }
+
+    const isPasswordCurrect = await  user.comparePassword(password);
+    if(!isPasswordCurrect) {
+        throw new UnauthenticatedError('Invalid credentials');
+    }
+
+    const tokenUser = { name: user.name, id: user._id, role: user.role };
+    createJwtAndVarify.attatchCookiesToResponse({res, user:tokenUser});
+    return res.status(StatusCodes.CREATED).json({ user: tokenUser});
+
 }
 
 
 module.exports.logout = (req, res) => {
-    return res.send('Log out user');
+    res.cookie('token', 'logout', {
+        httpOnly:true,
+        expires:new Date(Date.now()),
+    })
+
+    return res.status(StatusCodes.OK).json({msg:'user logout successfully'});
 }
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const port = 8000;
 require('express-async-errors');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./db/mongoose');
 const errorHandlerMiddleware = require('./middleware/error_handler');
 const notFound = require('./middleware/not_found');
@@ -11,6 +12,19 @@ const app = express();
 // middleware
 app.use(morgan('tiny'));
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+
+
+app.get('/', (req, res) => {
+    return res.send('E-commerce-api');
+})
+
+
+app.get('/api/v1', (req, res) => {
+    console.log(req.signedCookies);
+    return res.send('Cookie');
+})
 
 // routers
 app.use('/', require('./routes'));
